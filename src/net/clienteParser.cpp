851914b46301b8f64a9baed19d8df83831c8955a
Tomas Arjovsky <t.arjovsky@gmail.com>
2016-03-18 16:38:06 -0300

    compila parser y cliente

--- src/net/clienteParser.cpp
+++ src/net/clienteParser.cpp
@@ -23,8 +23,8 @@ void ClienteParser::serializador(Cliente *cliente, string ruta){
 	XMLDocument doc;
 
 	// Root
-	XMLNode * pRoot = doc.NewElement("Cliente");
-	doc.InsertFirstChild(pRoot);
+	XMLNode * pCliente = doc.NewElement("cliente");
+	doc.InsertFirstChild(pCliente);
 
 	// Hijos del root. TODO: diferencia entre element y node.
 	XMLElement * pNodoIp = doc.NewElement("ip");
@@ -38,57 +38,53 @@ void ClienteParser::serializador(Cliente *cliente, string ruta){
   // castear a string?
 
 	// NOTE: hacer la lista de punteros, para que no se copien mensajes, sino punteros.
-	for(list<Mensaje>::iterator iterador = cliente->getMensajes().begin(); iterador != cliente->getMensajes().end(); iterador++){
-		Mensaje mensaje = (*iterador);
+	for(list<Mensaje*>::iterator iterador = cliente->getMensajes().begin(); iterador != cliente->getMensajes().end(); iterador++){
+		XMLElement * pMensaje = doc.NewElement("mensaje");
+		Mensaje *mensaje =  (*iterador);
 
-		XMLElement * pListaMensajes = doc.NewElement("mensaje");
-		pListaMensajes-> SetText(mensaje);
+		XMLElement * pId = doc.NewElement("id");
+		pId-> SetText(mensaje->getId());
+		pMensaje ->InsertEndChild(pId);
 
-		XMLElement * pListaMensajes = doc.NewElement("id");
-		pListaMensajes-> SetText(mensaje.getId());
+		XMLElement * pTipo = doc.NewElement("tipo");
+		pTipo-> SetText(mensaje->getTipo());
+		pMensaje ->InsertEndChild(pTipo);
 
-		XMLElement * pListaMensajes = doc.NewElement("tipo");
-		pListaMensajes-> SetText(mensaje.getTipo());
-
-
-		XMLElement * pListaMensajes = doc.NewElement("valor");
+		XMLElement * pValor = doc.NewElement("valor");
 		// TODO ¿Quién debería encargarse de esto?
-		switch (mensaje.getTipo()) {
+		switch (mensaje->getTipo()) {
 			case T_STRING:{
-				string valor = (string) (*mensaje.getValor());
-				pListaMensajes-> SetText(valor);
+				string valor = *((string*) mensaje->getValor());
+				pValor-> SetText(valor.c_str());
 				break;
 			}
 			case T_INT:{
-				int valor = (int) (*mensaje.getValor());
-				pListaMensajes-> SetText(valor);
+				int valor = *((int*) mensaje->getValor());
+				pValor-> SetText(valor);
 				break;
 			}
 			case T_DOUBLE:{
-				double valor = (double) (*mensaje.getValor());
-				pListaMensajes-> SetText(valor);
+				double valor = *((double*) mensaje->getValor());
+				pValor-> SetText(valor);
 				break;
 				}
 			case T_CHAR:{
-				char valor = (char) (*mensaje.getValor());
-				pListaMensajes-> SetText(valor);
+				char valor = *((char*) mensaje->getValor());
+				pValor-> SetText(valor);
 				break;
 			}
 		}
 
-		pMensaje->InsertEndChild(pListaMensajes);
-		}
+		pMensaje->InsertEndChild(pValor);
 	}
 
-    (*pNodoMensajes).SetText(10);
+  // Inserto ambos elementos al root.
+  pCliente-> InsertFirstChild(pNodoIp);
+  pCliente-> InsertFirstChild(pNodoPort);
+  pCliente-> InsertFirstChild(pNodoMensajes);
 
-    // Inserto ambos elementos al root.
-    pRoot -> InsertFirstChild(pNodoIp);
-    pRoot -> InsertFirstChild(pNodoPort);
-    pRoot -> InsertFirstChild(pNodoMensajes);
-
-    XMLError e = doc.SaveFile("cliente.xml");
-    if (e != XML_SUCCESS){
-	    cout << "Error! " << e << endl;
-    }
+  XMLError e = doc.SaveFile("cliente.xml");
+  if (e != XML_SUCCESS){
+    cout << "Error! " << e << endl;
+  }
 }
