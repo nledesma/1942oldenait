45c1357f22fdbe063e24901a720034a9b13f9792
Rocio <rocio.impaglione@gmail.com>
2016-04-12 17:06:03 -0300

    Arreglado mainServidor para levantar el default en caso de XML invalido, o finalizar el programa en caso de XML default invalido.

--- app/mainServidor.cpp
+++ app/mainServidor.cpp
@@ -20,8 +20,26 @@ void* apagarServidor(void* servidor){
     pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]){
+void ejecutar(Servidor* servidor){
     pthread_t apagar;
+
+    //void* unServidor = (void*) servidor;
+    pthread_create(&apagar,NULL,apagarServidor,servidor);
+    // Servidor aceptando conexiones
+    try{
+        servidor->pasivar();
+    }catch(runtime_error &e){
+        Logger::instance()->logError(errno,"Se produjo un error en el listen");
+    }
+    //servidor->esperar();
+    //servidor->cerrar();
+    pthread_join(apagar, NULL);
+    Logger::instance()->cerrar();
+    //Logger::resetInstance();
+    pthread_exit(NULL);
+}    
+
+int main(int argc, char *argv[]){
     string rutaXMLServidor;
 
     if (argc < 2){
@@ -35,25 +53,28 @@ int main(int argc, char *argv[]){
     Servidor * servidor;
     ServidorParser servidorParser;
     servidor = servidorParser.deserializar(rutaXMLServidor);
-    if(servidor == NULL){
-     cout << "Error cargando el archivo provisto. Se procederá con la configuración por defecto." << endl;
-     servidor = servidorParser.deserializar(DEFAULT_XML);
+
+    if (servidor != NULL){
+        ejecutar(servidor);
+        delete servidor;
+        return 0;
+    }
+    
+    Logger::instance()->logWarning("Error cargando el archivo provisto. Se procederá con la configuración por defecto.")
+    cout << "Error cargando el archivo provisto. Se procederá con la configuración por defecto." << endl;
+    servidor = servidorParser.deserializar(DEFAULT_XML);
+
+    if (servidor != NULL){
+        ejecutar(servidor);
+        delete servidor;
+        return 0;
     }
 
-    //void* unServidor = (void*) servidor;
-    pthread_create(&apagar,NULL,apagarServidor,servidor);
-
-    // Servidor aceptando conexiones
-    try{
-        servidor->pasivar();
-    }catch(runtime_error &e){
-        Logger::instance()->logError(errno,"Se produjo un error en el listen");
-    }
-
-    //servidor->esperar();
-    //servidor->cerrar();
-    pthread_join(apagar, NULL);
-    Logger::instance()->cerrar();
-    //Logger::resetInstance();
-    pthread_exit(NULL);
+    Logger::instance()->logWarning("Hubo un error al inicializar el servidor desde el archivo XML por default");
+    cout << "Hubo un error al iniciar el servidor. Presione cualquier tecla para finalizar el programa" << endl;
+    cin.get();
+    
+    return 0;
 }
+
+
