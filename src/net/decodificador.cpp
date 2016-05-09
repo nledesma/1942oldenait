b33135a16adc75c518fec26a0687af03878eee76
Tomás Arjovsky <t.arjovsky@gmail.com>
2016-05-08 23:01:58 -0300

    agrego popFloat y popInt

--- src/net/decodificador.cpp
+++ src/net/decodificador.cpp
@@ -191,3 +191,11 @@ string Decodificador::getCodigoEstadoActual(EscenarioJuego *escenarioJuego) {
     }
     return codigo;
 }
+
+float Decodificador::popFloat(string & codigo) {
+    return stringToFloat(popBytes(codigo, sizeof(float)));
+}
+
+int Decodificador::popInt(string & codigo) {
+    return stringToInt(popBytes(codigo, sizeof(int)));
+}
