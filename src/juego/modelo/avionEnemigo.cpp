aa5ac554ab2c069ac155f48ba6bd9f651e752b62
nico <nicolas.angel.ledesma@gmail.com>
2016-05-22 17:46:23 -0300

    inicio de la elaboracion de la clase avion enemigo

---
+++ src/juego/modelo/avionEnemigo.cpp
@@ -0,0 +1,35 @@
+#include "avionEnemigo.hpp"
+
+AvionEnemigo::AvionEnemigo(float posX, float posY, float velocidad, float angulo, float velocidadDisparos, string idSprite, string idSpriteDisparos){
+    this->posX = posX;
+    this->posY = posY;
+    this->posXInicial = posX;
+    this->posYInicial = posY;
+    this->velocidadX = 0;
+    this->velocidadY = 0;
+    this->velocidad = velocidad;
+    this->velocidadDisparos = velocidadDisparos;
+    this->estadoAnimacion = ESTADO_NORMAL;
+    this->idSprite = idSprite;
+    this->idSpriteDisparos = idSpriteDisparos;
+}
+
+AvionEnemigo::~AvionEnemigo(){
+
+}
+
+float AvionEnemigo::getVelocidad(){
+    return this->velocidad;
+}
+
+float AvionEnemigo::getPosicionX(){
+    pthread_mutex_lock(&this->mutexMover);
+    float posicionX = this->posX;
+    pthread_mutex_unlock(&this->mutexMover);
+    return posicionX;
+}
+float AvionEnemigo::getPosicionY(){
+    pthread_mutex_lock(&this->mutexMover);
+    float posicionY = this->posY;
+    pthread_mutex_unlock(&this->mutexMover);
+    return posicionY;}
---
+++ src/juego/modelo/avionEnemigo.hpp
@@ -0,0 +1,76 @@
+#ifndef INC_1942OLDENAIT_AVIONENEMIGO_HPP
+#define INC_1942OLDENAIT_AVIONENEMIGO_HPP
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <list>
+#include <chrono>
+#include "../../accesorios/codigo.hpp"
+#include "disparo.hpp"
+#include <pthread.h>
+
+class AvionEnemigo {
+private:
+    float velocidadDisparos;
+    float posX;
+    float posY;
+    float velocidadX;
+    float velocidadY;
+    float angulo;
+    int estadoAnimacion;
+    float velocidad;
+    float posXInicial;
+    float posYInicial;
+    string idSprite;
+    string idSpriteDisparos;
+    pthread_mutex_t mutexMover = PTHREAD_MUTEX_INITIALIZER;
+
+public:
+    AvionEnemigo(float posX, float posY, float velocidad, float angulo, float velocidadDisparos, string idSprite, string idSpriteDisparos);
+
+    ~AvionEnemigo();
+
+    Disparo* disparar();
+
+    float getVelocidad();
+
+    float getPosicionX();
+
+    float getPosicionY();
+
+    float getAngulo();
+
+    int getEstadoAnimacion();
+
+    void setVelocidad(float velocidad);
+
+    void setVelocidadX();
+
+    void setVelocidadY();
+
+    void setPosicionX();
+
+    void setPosicionY();
+
+    void setEstadoAnimacion(int estadoAnimacion);
+
+    void mover(float timeStep);
+
+    int getAncho();
+
+    int getAlto();
+
+    int getAnchoDisparo();
+
+    int getAltoDisparo();
+
+    void volverEstadoInicial();
+
+    string getIdSprite();
+
+    string getIdSpriteDisparos();
+};
+
+
+#endif //INC_1942OLDENAIT_AVIONENEMIGO_HPP
