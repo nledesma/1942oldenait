f87aae4741dbc5dede880fb2f95d892f08bd1e87
nico <nicolas.angel.ledesma@gmail.com>
2016-05-29 04:23:54 -0300

    agregado de fabrica de enemigos al makefile

--- src/juego/modelo/trayectoriasEnemigos/fabricaDeEnemigos.hpp
+++ src/juego/modelo/trayectoriasEnemigos/fabricaDeEnemigos.hpp
@@ -1,6 +1,10 @@
-#include "avionEnemigo.hpp"
+#include "../avionEnemigo.hpp"
+#include "../avionPequenio.hpp"
+#include "../avionDeEscuadron.hpp"
+#include "../avionMediano.hpp"
+#include "../avionGrande.hpp"
 
 class FabricaEnemigos {
 public:
-    AvionEnemigo *fabricarEnemigo(string tipo, string cantidad);
+//    AvionEnemigo *fabricarEnemigo(string tipo, string cantidad);
 };
