ac6d29dcedbc33aa0e51c732843db1eafe529e1c
Tomás Arjovsky <t.arjovsky@gmail.com>
2016-06-05 17:04:40 -0300

    Arreglo segfault. Nota: hacer deletes o cerrar las cosas antes de la ventana.

--- app/mainOpciones.cpp
+++ app/mainOpciones.cpp
@@ -2,26 +2,29 @@
 #include "../src/juego/vista/ventana.hpp"
 int main(){
     Ventana ventana(800, 800);
-    ListaDeSeleccion lista(&ventana);
     ventana.iniciar();
-    bool quit = false;
-	SDL_Event e;
 
-    lista.agregarOpcion("Opcion 1");
-    lista.agregarOpcion("Opcion 2");
-    lista.agregarOpcion("Opcion 3");
+    ListaDeSeleccion* lista = new ListaDeSeleccion(&ventana);
+
+    lista->agregarOpcion("Opcion 1");
+    lista->agregarOpcion("Opcion 2");
+    lista->agregarOpcion("Opcion 3");
+
+    bool quit = false;
+    SDL_Event e;
 
     while( !quit ){
 		while( SDL_PollEvent( &e ) != 0 ){
-			if(e.type == SDL_QUIT){
+			if(e.type == SDL_QUIT) {
 				quit = true;
 			}
 		}
         ventana.limpiar();
-        lista.renderizar();
+        lista->renderizar();
         SDL_RenderPresent(ventana.getVentanaRenderer());
-
     }
+
+    delete lista;
     ventana.cerrar();
     return 0;
 }
